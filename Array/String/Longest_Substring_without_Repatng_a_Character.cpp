9e04dce24d049599110db0d0cb36ddfdb4250e6d
Ritwik <gritwik5843@gmail.com>
2025-08-05 16:41:08 +0530

    new file added

---
+++ Array/String/Longest_Substring_without_Repatng_a_Character.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    string s="bbbbbbbbbbbbbb";
+   vector<bool>count(256,0);
+int first =0,second=0,len=0;
+while(second<s.size()){
+    //repeating char
+    while(count[s[second]]){
+        count[s[first]]=0;
+         first++;
+    }
+    count[s[second]]=1;
+    len=(second-first+1);
+    second++;
+}
+cout<<len;
+}
--- Array/String/Sorting_The_Sentence.cpp
+++ Array/String/Sorting_The_Sentence.cpp
@@ -8,6 +8,7 @@ vector<string>ans(10);
 string temp;
 int index=0;
 int count=0;
+//we have to itreate over the last char so that why here <= not <;
 while(index<=s.size()){
     if(s[index]==' ' || s[index]=='\0'){
         int pos=temp[temp.size()-1]-'0';
@@ -27,6 +28,7 @@ for(int i=1;i<=count;i++){
     temp+=ans[i];
     temp+= " ";
 }
+//for removing last space
 temp.pop_back();
 cout<<temp;
 }
