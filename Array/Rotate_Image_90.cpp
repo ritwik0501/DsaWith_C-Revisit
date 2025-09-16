d65b35f43a26b84150f6e7d92650834611b2e4e6
Ritwik <gritwik5843@gmail.com>
2025-09-16 19:46:06 +0530

    add rotate image 90

---
+++ Array/Rotate_Image_90.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr1[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int arr2[4][4];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            int temp=3-i;
+            arr2[j][temp]=arr1[i][j] ;
+        }
+    }
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
